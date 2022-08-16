6683c665adeb0541cac2fe27917c0d60efd2d9ed
danilomarcello <danilo.marcello.s@gmail.com>
2022-08-16 17:35:02 -0300

    weapon card fix

--- src/components/WeaponCard.jsx
+++ src/components/WeaponCard.jsx
@@ -2,16 +2,12 @@ import React from 'react'
 
 const WeaponCard = ({ weapon }) => {
   return (
-<div class="card w- bg-base-100 shadow-xl">
-  <figure class="px-10 pt-10">
-    <img src={weapon.displayIcon} alt="" class="rounded-xl" />
-  </figure>
+<div className="card card-compact w-60 bg-base-100 shadow-xl m-10 cursor-pointer hover:shadow-lg transition-all duration-200 ease-in-out transform hover:-translate-y-2">
+  <figure><img className='max-h-20' src={weapon.displayIcon} alt={weapon.displayName} /></figure>
   <div class="card-body items-center text-center">
-    <h2 class="card-title">{weapon.displayName}</h2>
-
-    <div class="card-actions">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
+    <h2 className="card-title">{weapon.displayName}</h2>
+    <p>{weapon.shopData?.category}</p>
+    <p class="badge badge-outline">Price : {weapon.shopData?.cost} </p>
   </div>
 </div>
   )
