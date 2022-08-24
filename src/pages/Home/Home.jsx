f417b734b2583c9a49763c7fec5905e3dd5ffe17
Danilo <danilo.marcello.s@gmail.com>
2022-08-23 21:08:15 -0300

    fixing homepage text

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,13 +6,13 @@ import homeImg from '../../assets/agents-home.png'
 
 const Home = () => {
   return (
-    <div className='h-full w-full flex justify-center items-center flex-col font-pop'>
+    <div className='h-full w-full flex justify-center items-center flex-col text-center font-pop'>
       <Nav/>
       <img 
       className='m-3'
       src={homeImg}/>
       <h1 className='text-5xl m-2'>Valorant Wiki</h1>
-      <h2 className='text-xl m-2'>Here you will find all the Agents, Weapons and Maps from Valorant.</h2>
+      <h2 className='text-sm my-2'>VALORANT is a free-to-play first-person tactical shooter video game being developed and published by Riot Games.</h2>
     </div>
   )
 }
